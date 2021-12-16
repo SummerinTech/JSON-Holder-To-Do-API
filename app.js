0324235e6770da389b7949fdcfbb1beb6cc9443e
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-12-15 19:07:05 -0800

    update app structure

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//makes call to api and returns response
 async function loadToDos() {
 	const url = "https://jsonplaceholder.typicode.com/todos/";
 
@@ -6,9 +7,47 @@ async function loadToDos() {
 	return jsonResponse;
 }
 
+//receives data and filters out the number of arrays sent through API
 async function modifyToDos() {
 	const data = await loadToDos();
-	console.log(data);
+	const todos = await data;
+	let filtered = todos.filter((item, index) => {
+		return index < cardsStructure.length;
+	});
+	return filtered;
 }
 
-modifyToDos();
+//add data to our HTML file
+async function addApi() {
+	const arr = await modifyToDos();
+	let data = arr.map((values) => {
+		return {
+			title: values.title,
+			id: values.id,
+			completed: values.completed,
+		};
+	});
+	data.map((item, i) => {
+		cards.cardTitles[i].innerHTML = item.title;
+		cards.numberId[i].innerHTML = item.id;
+		item.completed
+			? (cards.completed[i].innerHTML = "Completed ✅")
+			: (cards.completed[i].innerHTML = "Due Soon 🚨");
+	});
+}
+
+//HTML Structures for card components
+const cardsStructure = document.querySelectorAll(".card");
+const cardTitles = document.querySelectorAll(".title-text");
+const completed = document.querySelectorAll(".completed-text");
+const numberId = document.querySelectorAll(".id");
+
+//Made the Structures into an objet
+const cards = {
+	cardTitles: cardTitles,
+	completed: completed,
+	numberId: numberId,
+};
+
+//calling API functions
+addApi();
